e9af49efaf1ece6357ea4860808951867f3c7034
MichelleEllyana <michelleellyanatedjo_2226250032@mhs.mdp.ac.id>
2023-10-02 18:27:36 +0700

    first commit

---
+++ void main () {.dart
@@ -0,0 +1,14 @@
+void main () {
+  // variabel String untuk teks pertama
+  String teks1 = "Selamat";
+
+  // variabel String untuk teks kedua
+  String teks2 = "Pagi";
+
+  // menggabungkan teks1 dan teks2 kedalam teks3
+  String teks3 = teks1 + " " + teks2
+
+  // mencetak teks3 ke konsol
+  print(teks3);
+   
+}
